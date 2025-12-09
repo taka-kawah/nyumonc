abc570acb6c341bac99c08263e8f40fb033cd490
河原 <106893036+taka-kawah@users.noreply.github.com>
2025-12-09 21:00:14 +0900

    九九

---
+++ q6/kuku.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    for (size_t i = 1; i < 10; i++)
+    {
+        for (size_t j = 1; j < 10; j++)
+        {
+            char buf[3];
+            snprintf(buf, sizeof(buf), "%2zu", i * j);
+            printf("%s", buf);
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
